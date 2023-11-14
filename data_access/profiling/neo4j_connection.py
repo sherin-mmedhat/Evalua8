f4990dd207e26888a849512610b41addfb1fe400
sherin-mmedhat <sherin.medhat@trianglz.com>
2023-11-14 15:28:50 +0200

    - enhance the neo4j connection shared method - add repository methods to get employee details , list of employees that the evaluator can evaluate , the tree hierarchy needed for teams -level= employees ordered by level - migration update

--- data_access/profiling/neo4j_connection.py
+++ data_access/profiling/neo4j_connection.py
@@ -1,16 +1,32 @@
 from neo4j import GraphDatabase
 from decouple import config
 
-def connect_to_neo4j():
-    uri = config('NEO4J_URI')
-    user = config('NEO4J_USERNAME')
-    password = config('NEO4J_PASSWORD')
-    
-    driver = GraphDatabase.driver(uri, auth=(user, password))
-    return driver
+class Neo4jConnector:
+    def __init__(self):
+        self._uri = config('NEO4J_URI')
+        self._user = config('NEO4J_USERNAME')
+        self._password = config('NEO4J_PASSWORD')
+        self._driver = None
 
-def execute_cypher_query(query,parameters=None):
-    with connect_to_neo4j() as driver:
-        with driver.session() as session:
-            result = session.run(query,parameters)
-            return result
+    def connect(self):
+        self._driver = GraphDatabase.driver(self._uri, auth=(self._user, self._password))
+    
+    def close(self):
+        if self._driver is not None:
+            self._driver.close()
+    
+    def find_by(self, query, parameters=None):
+        with self._driver.session() as session:
+            response = session.run(query, parameters)
+            if response:
+                result =response.single()
+                if result: 
+                    return result.data()
+            
+            return None
+    def find_all(self, query, parameters=None):
+        with self._driver.session() as session:
+            response = session.run(query, parameters)
+            if response:
+                return  [record.data() for record in response]          
+            return None        
--- data_access/profiling/repository/EmployeeRepository.py
+++ data_access/profiling/repository/EmployeeRepository.py
@@ -1,24 +1,67 @@
-import neo4j_connection
+from neo4j_connection import Neo4jConnector
 
+from decouple import config
 from neo4j import GraphDatabase
 
 def get_employee_details(employee_id):
-    
+
+    connector = Neo4jConnector()
+    connector.connect()
     query =  """
-       MATCH (e:Employee)-[:MEMBER_OF]->(s:Squad)-[OWNS]->(p:Project)
-       WHERE ID(e) = 3
-       RETURN e.name AS employee_name, COLLECT(Distinct s.name) AS squads , COLLECT(DISTINCT p.name) AS projects
+       MATCH (e:Employee {id: $employeeId})
+       OPTIONAL MATCH (e)-[:MEMBER_OF]->(s:Squad)-[:OWNS]->(p:Project)
+       RETURN e AS employee, COLLECT(DISTINCT p.name) AS projects, COLLECT(DISTINCT s.name) AS squads
         """
+    employee_data = connector.find_by(query,{'employeeId':employee_id})
     
-    result = neo4j_connection.execute_cypher_query(query, {'employee_id': employee_id})
-    data = result.single()
-    print(result)
-    return data
-    # if data:
-    #     return {
-    #         'employee_name': data['name'],
-    #         'squads': data['squads'],
-    #         'projects': data['projects'],
-    #     }
-    # else:
-    #     return None
+    connector.close()
+    if employee_data:
+        return employee_data
+    else:
+        return None
+
+def get_employees_hirerachy():
+
+    connector = Neo4jConnector()
+    connector.connect()
+    query =  """
+        MATCH (e:Employee)-[:BELONGS_TO]->(t:Team)
+        WITH e, t, e.level AS employeeLevel
+        ORDER BY employeeLevel desc
+        WITH t.name AS team,e.level AS level, COLLECT({ id: e.id, name: e.name, level: e.level , job_title: e.job_title }) AS employees
+        RETURN team, COLLECT({level: level, employees: employees}) AS employees
+        ORDER BY team 
+        """
+    employee_data = connector.find_all(query)
+    
+    connector.close()
+    if employee_data:
+        return employee_data
+    else:
+        return None
+
+def get_employees_to_evaluate(evaluatorId):
+    connector = Neo4jConnector()
+    connector.connect()
+    query =  """
+        MATCH (employee:Employee {id: $evaluatorId})
+        OPTIONAL MATCH (employee)-[:MENTOR]->(mentee:Employee)
+        OPTIONAL MATCH (employee)-[r1:MEMBER_OF]->(s:Squad)<-[r2:MEMBER_OF]-(colleague:Employee)
+        WITH employee, 
+        COLLECT(DISTINCT mentee {.*}) AS mentees,
+        COLLECT(DISTINCT colleague {.*}) AS colleagues
+        OPTIONAL MATCH (mentor:Employee)-[:MENTOR]->(employee)
+        RETURN employee.id AS employeeId, employee.name AS employeeName, mentees,colleagues,
+        COLLECT(DISTINCT mentor {.*}) AS mentors
+        """
+    employee_data = connector.find_all(query,{'evaluatorId':evaluatorId})
+    
+    connector.close()
+    if employee_data:
+        return employee_data
+    else:
+        return None
+
+
+
+    
