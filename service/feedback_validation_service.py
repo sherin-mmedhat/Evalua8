a368749932d9a69a84dcd1af2b3481049d3d99ef
sherin-mmedhat <sherin.medhat@trianglz.com>
2023-11-30 23:24:28 +0200

    validation feedback changes and testing script , changes to migration script

--- service/feedback_validation_service.py
+++ service/feedback_validation_service.py
@@ -3,45 +3,64 @@ import os
 import json
 
 from decouple import config
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 # from langchain.llms import OpenAI
 # from langchain import PromptTemplate, LLMChain
 
 
 class FeedbackValidationService: 
     def __init__(self):
-        self._open_pi_key = config('OPEN_AI_KEY')
+        self._open_Api_key = config('OPEN_API_KEY')
+        
+
 
     def get_completion(self,prompt, model="gpt-3.5-turbo"):
-        client = OpenAI(api_key=self._open_pi_key)
+        client = OpenAI(api_key=self._open_Api_key)
         
         messages = [{"role": "user", "content": prompt}]
         response = client.chat.completions.create(
             model=model,
             messages=messages,
-            temperature=0,
+            temperature=0.2,
         )
         return response
         
     def are_feedbacks_sufficient(self,feedbacks, kpis, questions):
     
-        prompt = f"""Analyze the employee feedback for clarity, specificity, and informativeness. Ensure examples of skills are given and the performance assessment questions are addressed thoroughly, it may be adressed in a negative or positive way which will be considered sufficient in both cases.Feedback:{', '.join(feedbacks)}
-KPI Categories:{', '.join(kpis)}
-Question:{{"questions": {json.dumps(questions)}}}
-Confirm feedback alignment with employee's KPIs for objective evaluation. Offer improvement suggestions for feedback lacking clarity or specificity
-Response Format: JSON object with analysis, sufficiency status, and suggestions for each question if and only if the sufficiency status is false, referenced by question ID. Example:
-[{{"question_id": 123, "is_sufficient": "true/false", "suggestions": "brief analysis"}}]"""
+        template = f"""Analyze the employee feedback for clarity, specificity, and informativeness that should address the KPIs and the questions given. Analyze each question against each feedback and check if the feedback covers the answer to the questions given, both in a positive and negative context.
 
+Format the output as JSON with the following keys:
+- question: each question from the given questions
+- is_sufficient: if the positive or negative aspects of the feedback are sufficient to answer the question; answer True if yes, False
+- suggestions: Offer improvement suggestions for each question that the feedback lacks clarity or specificity. Explicitly mention that examples have already been provided and ask for more specific details or instances.
 
+Feedback: [{', '.join(feedbacks)}]
+KPI Categories: {[', '.join(kpis)]}
+Questions: {[', '.join(questions)]}
+
+ if feedback was negative and contain a specifc reason so consider sufficient
+example : 
+feedback: employee x deosnt work quite well in team because he is rude to other employees , should ne considered as sufficient"""
+
+        prompt_template = ChatPromptTemplate.from_template(template)
+        messages = prompt_template.format_messages(feedbacks=feedbacks,kpis =kpis, questions= questions)
+        chat = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo",openai_api_key=self._open_Api_key)
+        response = chat(messages)
+        print(response.content)
         # prompt = PromptTemplate(template = template)
         # #prompt_formated= prompt.format(feedbacks=feedbacks, kpis= kpis, questions= questions)
         # # chain1 = LLMChain(llm=llm,prompt=prompt_formated)
         # # chain1.run(feedbacks,kpis,questions)
         # print(f"LLM Output: {llm(prompt)}")
-        response = self.get_completion(prompt)
-        print(response)
-        if response and response.choices[0]:
-           return response.choices[0].message.content
+        # response = self.get_completion(prompt)
+        #print(response)
+        # if response and response.choices[0]:
+        #    return response.choices[0].message.content
+        # else: 
+        #   return None
+        if response and response.content:
+            return response.content
         else: 
-          return None
-        
+           return None 
     
