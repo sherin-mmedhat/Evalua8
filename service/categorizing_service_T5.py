ba5fc64e458235c27cc6db4be346ce435822bb49
JolieSamehh <120934110+JolieSamehh@users.noreply.github.com>
2023-12-07 19:20:33 +0200

    Doesn't belong to category T5 update

--- service/categorizing_service_T5.py
+++ service/categorizing_service_T5.py
@@ -11,18 +11,25 @@ class CategorizingUsingTransformersService:
         category_str = ", ".join(map(str, categories))
         
         for text in text_samples:
-            input_text = f"{text}; Classify this sentence as {category_str} in one word."
-            input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
-            outputs = self.model.generate(input_ids)
+            input_text = f"Classify the following sentence: '{text}' as one of the following: {category_str}."
+            input_ids = self.tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
+            outputs = self.model.generate(input_ids, max_length=50)
             label = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-            labels.append(label)
+
+            # Check if the label contains any of the specified categories
+            predicted_categories = [category for category in categories if category.lower() in label.lower()]
+            
+            if predicted_categories:
+                labels.append(predicted_categories)
+            else:
+                labels.append(['Does not belong to any category'])
         
         return labels
 
 # Example usage
 categorizing_service = CategorizingUsingTransformersService()
-categories = ["Advanced Productivity", "Teamwork"]
-text_samples = ["I tried contacting her but she didn't reply for 2 days and this was blocking me for 2 days and made me late"]
+categories = ["Productivity", "Teamwork"]
+text_samples = ["She is good", "She delivers all tasks on time", "I tried contacting her but she didn't reply for 2 days and this was blocking me for 2 days and made me late"]
 
 # Call the method with the desired arguments
 category_labels = categorizing_service.categorize_text(categories, text_samples)
