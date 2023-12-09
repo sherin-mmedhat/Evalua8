298ff3ba5b33ccd013c29b24a6e6c76d4657972e
JolieSamehh <120934110+JolieSamehh@users.noreply.github.com>
2023-12-09 14:57:07 +0200

    Integrate category service in feedback service

--- service/feedback_service.py
+++ service/feedback_service.py
@@ -15,7 +15,9 @@ class FeedbackService:
         kpi_data_list = kpi_service.find_by_job_title(employee_details.title_code)
 
         kpis = set([kpi["KPI"] for kpi in kpi_data_list])
-        ##todo take kpis list and  feedback as inputs call categorise method , that will be used in kpis variable in submit_feedback
+        
+        category_label = categorizing_service.categorize_text(kpis, feedback)
+        
         feedback_id = feedback_repository.submit_feedback(evaluator_id=evaluator_id, feedback=feedback,
                                                           kpis=kpis)
         employee_repository.create_feedback_relation(employee_id=employee_id, feedback_id=feedback_id)
